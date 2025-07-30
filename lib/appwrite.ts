8d2716496eb465c0bba50e596f18910a94419c0c
Ranjithbabu0912 <ranjithbabu0912@gmail.com>
2025-07-30 20:16:54 +0530

    implement search and filter

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,12 +1,18 @@
-import { CreateUserPrams, SignInParams } from "@/type";
-import { Account, Avatars, Client, Databases, ID, Query } from "react-native-appwrite"
+import { Account, Avatars, Client, Databases, ID, Query, Storage } from "react-native-appwrite";
+import { Category, CreateUserPrams, GetMenuParams, SignInParams } from "@/type";
+
 
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
-    plaatform: "com.ranjith.foodordering",
+    platform: "com.ranjith.foodordering",
     databaseId: '6888854f0013108e9a9f',
-    userCollectionId: '6888857c002c9da39cc9'
+    bucketId: '6888e6510020a6e08f29',
+    userCollectionId: '6888857c002c9da39cc9',
+    categoriesCollectionId: '6888e20f002b8ac91ba7',
+    menuCollectionId: '6888e2b9000615a92cd4',
+    customizationsCollectionId: '6888e3cb0012167fc5cc',
+    menuCustomizationsCollectionId: '6888e4ba002093732724'
 }
 
 export const client = new Client();
@@ -14,17 +20,20 @@ export const client = new Client();
 client
     .setEndpoint(appwriteConfig.endpoint)
     .setProject(appwriteConfig.projectId)
-    .setPlatform(appwriteConfig.plaatform)
-
+    .setPlatform(appwriteConfig.platform)
 
 export const account = new Account(client);
 export const databases = new Databases(client);
+export const storage = new Storage(client);
 const avatars = new Avatars(client);
+
+
 
 export const createUser = async ({ email, password, name }: CreateUserPrams) => {
     try {
-        const newAccount = await account.create(ID.unique(), email, password, name);
+        const newAccount = await account.create(ID.unique(), email, password, name)
         if (!newAccount) throw Error;
+
         await signIn({ email, password });
 
         const avatarUrl = avatars.getInitialsURL(name);
@@ -33,7 +42,7 @@ export const createUser = async ({ email, password, name }: CreateUserPrams) => 
             appwriteConfig.databaseId,
             appwriteConfig.userCollectionId,
             ID.unique(),
-            { name, email, accountId: newAccount.$id, avatar: avatarUrl }
+            { email, name, accountId: newAccount.$id, avatar: avatarUrl }
         );
     } catch (e) {
         throw new Error(e as string);
@@ -62,9 +71,40 @@ export const getCurrentUser = async () => {
         if (!currentUser) throw Error;
 
         return currentUser.documents[0];
-
     } catch (e) {
         console.log(e);
+        throw new Error(e as string);
+    }
+}
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+    try {
+        const queries: string[] = [];
+
+        if (category) queries.push(Query.equal('categories', category));
+        if (query) queries.push(Query.search('name', query));
+
+        const menus = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.menuCollectionId,
+            queries,
+        )
+
+        return menus.documents;
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
+
+export const getCategories = async () => {
+    try {
+        const categories = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.categoriesCollectionId,
+        )
+
+        return categories.documents as unknown as Category[];
+    } catch (e) {
         throw new Error(e as string);
     }
 }
